1db15a5f1ca66fa3d163907a8a9a07b1756bd89c
Igor <igordev96@gmail.com>
2022-01-10 00:07:18 +0000

    little change on a state

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   //Ref
   const audioRef = useRef(null);
   //State
-  const [songs, setSongs] = useState(data());
+  const songs = data();
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
   const [libraryStatus, setLibraryStatus] = useState(false);
